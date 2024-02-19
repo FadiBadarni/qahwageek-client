a80ae6263c0b1d7e07ced8b0d740597ec947288f
FadiBadarni <fadybd1@gmail.com>
2024-02-19 19:29:02 +0200

    Refactor PostItem component styling and resposnive design

--- src/components/shared/PostItem.tsx
+++ src/components/shared/PostItem.tsx
@@ -30,26 +30,27 @@ const PostItem: React.FC<PostItemProps> = ({ post }) => {
   };
   return (
     <div
-      className="flex items-center p-4 border-b border-gray-300 last:border-b-0 dark:border-dark-500 bg-gray-100 dark:bg-dark-700 rounded-md cursor-pointer transition duration-300 ease-in-out transform hover:-translate-y-1 hover:shadow-lg hover:bg-gray-200 dark:hover:bg-dark-600"
+      className="flex items-center p-4 pb-0 border-b border-gray-300 last:border-b-0 dark:border-dark-500 bg-gray-100 dark:bg-dark-700 rounded-md cursor-pointer transition duration-300 ease-in-out transform hover:-translate-y-1 hover:shadow-lg hover:bg-gray-200 dark:hover:bg-dark-600"
       onClick={handleClick}
     >
       <div className="flex-grow flex flex-col justify-between h-full">
         {/* Title and category badges container */}
-        <div className="flex justify-between items-start">
-          <h3 className="text-base sm:text-lg font-semibold dark:text-white">
+        <div className="flex flex-col justify-start items-start">
+          <h3 className="text-base sm:text-lg lg:text-lg font-semibold dark:text-white mb-2 line-clamp-2">
             {title}
           </h3>
-          <div className="flex flex-wrap gap-2">
+          <div className="flex flex-wrap gap-2 w-full">
             {categoryNames.map((category, index) => (
               <span
                 key={index}
-                className="inline-flex items-center rounded-md bg-gray-400/10 px-2 py-1 text-xs font-medium text-gray-400 ring-1 ring-inset ring-gray-400/20"
+                className="inline-flex items-center justify-center rounded-md bg-gray-400/10 px-2 py-1 text-xs sm:text-xs font-medium text-gray-400 ring-1 ring-inset ring-gray-400/20"
               >
                 {category}
               </span>
             ))}
           </div>
         </div>
+
         <div className="flex justify-between items-center text-gray-500 dark:text-gray-400 text-xs sm:text-sm mt-4 h-12">
           <div className="flex flex-col sm:flex-row items-center">
             <UserIcon className="h-4 w-4 sm:h-5 sm:w-5 mb-1 sm:mb-0 sm:ml-2" />
