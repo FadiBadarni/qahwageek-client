b550de48422877c772eb78dd3eaef20fc909537f
FadiBadarni <fadybd1@gmail.com>
2024-02-28 17:22:54 +0200

    added helmet SEO for the user profile page.

--- src/components/user/index.tsx
+++ src/components/user/index.tsx
@@ -4,15 +4,15 @@ import { useSelector } from 'react-redux';
 import { RootState } from 'store/store';
 import { getUserProfile } from 'store/user/userActions';
 import UserPosts from './UserPosts';
-import { format, parseISO } from 'date-fns';
-import { ar } from 'date-fns/locale';
-import UserSocialMedia from './UserSocialMedia';
+import UserBasicInfo from './UserBasicInfo';
+import { Helmet } from 'react-helmet';
 
 const UserProfile: React.FC = () => {
   const dispatch = useAppDispatch();
   const userProfile = useSelector((state: RootState) => state.userProfile.data);
   const status = useSelector((state: RootState) => state.userProfile.status);
   const userId = useSelector((state: RootState) => state.user.data?.id);
+
   useEffect(() => {
     if (userId) {
       dispatch(getUserProfile(userId));
@@ -29,45 +29,45 @@ const UserProfile: React.FC = () => {
 
   return (
     <div className="max-w-7xl mx-auto p-4">
+      <Helmet>
+        <title>{`${userProfile.username} - المساحة الشخصية`}</title>
+        <meta
+          name="description"
+          content={`${
+            userProfile.bio ||
+            'استكشف المساحة الشخصية والمقالات ل' + userProfile.username
+          } في قهوة چيك.`}
+        />
+        <meta
+          property="og:title"
+          content={`${userProfile.username} - المساحة الشخصية`}
+        />
+        <meta
+          property="og:description"
+          content={`${
+            userProfile.bio ||
+            'استكشف المساحة الشخصية والمقالات ل' + userProfile.username
+          } في قهوة چيك.`}
+        />
+        <meta
+          property="og:image"
+          content={
+            userProfile.profilePicture || 'URL_TO_A_DEFAULT_PROFILE_IMAGE'
+          }
+        />
+        <meta property="og:type" content="profile" />
+        <meta
+          property="og:url"
+          content={`https://qahwageek.netlify.app/user/${userId}`}
+        />
+        <link
+          rel="canonical"
+          href={`https://qahwageek.netlify.app/user/${userId}`}
+        />
+      </Helmet>
       <div className="flex flex-col md:flex-row gap-4">
         {/* User's Basic Information */}
-        <div className="md:flex-1 md:w-1/3 flex flex-col items-center text-center p-4">
-          <div className="h-32 w-32 md:h-48 md:w-48 overflow-hidden rounded-full border-4 border-primary self-center">
-            {userProfile.profilePicture ? (
-              <img
-                src={userProfile.profilePicture}
-                alt="الملف الشخصي"
-                className="object-cover"
-              />
-            ) : (
-              <div className="flex items-center justify-center h-full w-full bg-neutral-400 text-white">
-                {userProfile.username.charAt(0)}
-              </div>
-            )}
-          </div>
-          <h2 className="mt-2 text-lg sm:text-xl md:font-bold dark:text-text text-dark-text">
-            {userProfile.username}
-          </h2>
-          <p className="text-xs sm:text-sm dark:text-neutral-200">
-            {userProfile.email}
-          </p>
-          <p className="mt-1 text-xs sm:text-sm dark:text-neutral-400">
-            {userProfile.bio || 'لا يوجد سيرة ذاتية'}
-          </p>
-          <p className="mt-1 text-xs sm:text-sm dark:text-neutral-200">
-            انضم:{' '}
-            {format(parseISO(userProfile.joinDate), 'dd MMMM, yyyy', {
-              locale: ar,
-            })}
-          </p>
-          {/* Subtitle for Social Media Handles */}
-          <h3 className="mt-3 mb-1 text-xs sm:text-sm font-semibold dark:text-dark-text text-light-text">
-            حسابات التواصل الاجتماعي
-          </h3>
-          <UserSocialMedia
-            socialMediaHandles={userProfile.socialMediaHandles}
-          />
-        </div>
+        <UserBasicInfo userProfile={userProfile} />
 
         {/* User's Posts */}
         <div className="md:flex-2 md:w-2/3 space-y-4">
