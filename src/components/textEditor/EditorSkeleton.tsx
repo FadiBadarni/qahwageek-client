d00364ad20f2ef802541853b74f50b8cc5b6830c
FadiBadarni <fadybd1@gmail.com>
2024-03-04 09:27:06 +0200

    added tinymce loading skeleton

---
+++ src/components/textEditor/EditorSkeleton.tsx
@@ -0,0 +1,7 @@
+const EditorSkeleton: React.FC = () => (
+  <div className="animate-pulse flex flex-col space-y-4 p-4">
+    <div className="h-96 bg-neutral-200 dark:bg-neutral-700 rounded-md"></div>
+  </div>
+);
+
+export default EditorSkeleton;
