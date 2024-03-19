51876aff75c5f67b71c5309db87250d3902a73db
FadiBadarni <fadybd1@gmail.com>
2024-03-19 10:52:16 +0200

    Update EventCard component and EventsPage layout

--- src/components/event/EventCard.tsx
+++ src/components/event/EventCard.tsx
@@ -2,7 +2,11 @@ import React from 'react';
 import { format, parseISO } from 'date-fns';
 import { ar } from 'date-fns/locale';
 import { MeetupEvent } from 'models/event';
-import { GlobeAltIcon } from '@heroicons/react/24/outline';
+import {
+  CalendarIcon,
+  GlobeAltIcon,
+  MapPinIcon,
+} from '@heroicons/react/24/outline';
 
 interface EventCardProps {
   event: MeetupEvent;
@@ -11,55 +15,51 @@ interface EventCardProps {
 
 const EventCard: React.FC<EventCardProps> = ({ event, onViewDetails }) => {
   return (
-    <div className="flex flex-col rounded-lg shadow-lg overflow-hidden bg-white dark:bg-dark-layer transition duration-300 ease-in-out transform hover:-translate-y-1 hover:shadow-xl relative group">
-      {event.onlineEvent && (
-        <div className="absolute right-2 top-2 z-20 flex items-center justify-center h-8 w-8 bg-brand-500 text-white rounded-full">
-          <GlobeAltIcon className="h-5 w-5" aria-hidden="true" />
-        </div>
-      )}
+    <div className="flex flex-col rounded-lg shadow-lg overflow-hidden bg-white dark:bg-dark-layer transition duration-300 ease-in-out transform hover:-translate-y-1 hover:shadow-xl relative">
       <div className="flex-shrink-0">
         <img
-          className="h-48 w-full object-cover group-hover:opacity-75 transition-opacity duration-300 ease-in-out"
+          className="h-40 w-full object-cover"
           src={event.imageUrl || '/default-event-image.jpg'}
           alt={event.title}
         />
       </div>
-      <div className="flex-1 p-6 space-y-3 flex flex-col justify-between">
+      <div className="flex-1 p-4 flex flex-col justify-between">
         <div>
-          <p className="text-sm font-medium text-brand-400 dark:text-accent-400">
-            {event.category.name}
-          </p>
           <h3 className="text-xl font-semibold text-gray-900 dark:text-white">
             {event.title}
           </h3>
-          <p className="text-base text-gray-500 dark:text-gray-400 line-clamp-3">
-            {event.description.length > 100
-              ? `${event.description.substring(0, 100)}...`
-              : event.description}
+          <p className="mt-3 text-base text-gray-500 dark:text-gray-400 line-clamp-2">
+            {event.description}
           </p>
         </div>
-        <div>
-          <div className="text-sm text-gray-500 dark:text-gray-400">
+        <div className="mt-2">
+          <div className="flex items-center text-sm text-gray-500 dark:text-gray-400 space-x-2">
+            <CalendarIcon className="h-5 w-5 text-neutral-500 dark:text-neutral-400  ml-2" />
             <time dateTime={event.dateTime}>
-              {format(parseISO(event.dateTime), 'd MMMM yyyy', {
-                locale: ar,
-              })}
+              {format(parseISO(event.dateTime), 'd MMMM yyyy', { locale: ar })}
             </time>
-            {!event.onlineEvent && event.location && (
+            {event.onlineEvent ? (
               <>
-                <span aria-hidden="true"> · </span>
-                <span>{event.location}</span>
+                <GlobeAltIcon className="h-5 w-5 text-neutral-500 dark:text-neutral-400" />
+                <span>عبر الإنترنت</span>
               </>
+            ) : (
+              event.location && (
+                <>
+                  <MapPinIcon className="h-5 w-5 text-neutral-500 dark:text-neutral-400" />
+                  <span>{event.location}</span>
+                </>
+              )
             )}
           </div>
-          <button
-            className="mt-4 w-full bg-brand-500 hover:bg-brand-600 text-white font-bold py-2 rounded-md transition duration-300 ease-in-out"
-            onClick={() => onViewDetails(event)}
-          >
-            المزيد من التفاصيل
-          </button>
         </div>
       </div>
+      <button
+        className="w-full bg-brand-500 hover:bg-brand-600 text-white font-bold py-2 rounded-md transition duration-300 ease-in-out"
+        onClick={() => onViewDetails(event)}
+      >
+        المزيد من التفاصيل
+      </button>
     </div>
   );
 };
--- src/components/event/index.tsx
+++ src/components/event/index.tsx
@@ -37,8 +37,8 @@ const EventsPage: React.FC = () => {
   };
 
   return (
-    <div className="max-w-7xl mx-auto py-8 px-4 sm:px-6 lg:px-8">
-      <div className="text-center mb-12">
+    <div className="max-w-7xl mx-auto py-4 px-4 sm:px-6 lg:px-8">
+      <div className="text-center mb-6">
         <h2 className="text-3xl font-extrabold text-brand-500 dark:text-accent-500">
           كل الأحداث
         </h2>
