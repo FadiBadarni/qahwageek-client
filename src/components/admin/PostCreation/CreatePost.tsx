c3174a85db3f64f2ae8c81af34e350e4c67c3cb8
FadiBadarni <fadybd1@gmail.com>
2024-03-21 17:14:41 +0200

    Add react-dropzone package and implement image upload functionality

--- src/components/admin/PostCreation/CreatePost.tsx
+++ src/components/admin/PostCreation/CreatePost.tsx
@@ -1,5 +1,5 @@
 import { useAppDispatch } from 'hooks/useAppDispatch';
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { RootState } from 'store/store';
 import CategorySelect from './CategorySelect';
@@ -13,6 +13,7 @@ import {
 import { fetchAllCategories } from 'store/category/categoryActions';
 import { displayToast } from 'utils/alertUtils';
 import { useNavigate } from 'react-router-dom';
+import { useDropzone } from 'react-dropzone';
 
 const CreatePost = () => {
   const dispatch = useAppDispatch();
@@ -27,6 +28,8 @@ const CreatePost = () => {
   const [selectedImage, setSelectedImage] = useState<File | null>(null);
   const [selectedCategoryIds, setSelectedCategoryIds] = useState<number[]>([]);
   const [loading, setLoading] = useState(false);
+
+  const [imagePreviewUrl, setImagePreviewUrl] = useState<string | null>(null);
 
   useEffect(() => {
     dispatch(fetchAllCategories());
@@ -72,11 +75,23 @@ const CreatePost = () => {
     }
   };
 
+  const onDrop = useCallback((acceptedFiles: File[]) => {
+    const file = acceptedFiles[0];
+    setSelectedImage(file);
+    setImagePreviewUrl(URL.createObjectURL(file));
+  }, []);
+
+  const { getRootProps, getInputProps } = useDropzone({
+    accept: 'image/*' as any,
+    onDrop,
+  });
+
   return (
     <div className="p-4 mx-auto max-w-7xl px-6 lg:px-8">
       <form onSubmit={handleSubmit} className="space-y-4">
-        <div className="grid grid-cols-1 md:grid-cols-6 gap-4 items-end">
-          <div className="md:col-span-6 lg:col-span-4">
+        <div className="grid grid-cols-1 md:grid-cols-6 gap-4">
+          {/* Title input */}
+          <div className="md:col-span-4">
             <label
               htmlFor="title"
               className="block text-sm font-medium text-light-text dark:text-dark-text mb-2"
@@ -93,32 +108,41 @@ const CreatePost = () => {
               className="mt-1 block w-full rounded-md border border-neutral-300 bg-light-input dark:bg-dark-input py-2 px-4 placeholder-neutral-400 focus:bg-white focus:text-neutral-900 dark:placeholder:text-neutral-500 dark:focus:bg-dark-800 dark:focus:text-neutral-100 focus:outline-none focus:ring-2 focus:ring-brand-500"
               required
             />
-          </div>
-          <div className="md:col-span-6 lg:col-span-2">
-            <label
-              htmlFor="image"
-              className="block text-sm font-medium text-light-text dark:text-dark-text mb-2"
-            >
-              اختر صورة الغلاف
-            </label>
-            <input
-              id="image"
-              type="file"
-              accept="image/*"
-              onChange={(e) =>
-                setSelectedImage(e.target.files ? e.target.files[0] : null)
-              }
-              className="block w-full text-sm file:py-2 file:px-4 file:rounded-md file:border dark:file:border-neutral-700 file:text-sm file:font-semibold file:bg-light-input file:text-neutral-700 hover:file:bg-light-200 dark:file:bg-dark-input dark:file:text-neutral-200 dark:file:hover:bg-dark-layer focus:outline-none focus:ring-2 focus:ring-brand-500"
+            <CategorySelect
+              categories={categories}
+              selectedCategoryIds={selectedCategoryIds}
+              onCategoryChange={setSelectedCategoryIds}
             />
           </div>
+
+          {/* Dropzone for image upload */}
+          <div className="md:col-span-6 lg:col-span-2 flex flex-col">
+            <label className="text-sm font-medium text-light-text dark:text-dark-text mb-2">
+              صورة الغلاف
+            </label>
+            <div
+              {...getRootProps({
+                className:
+                  'dropzone flex flex-col justify-center items-center border-dashed border-2 border-gray-300 rounded-lg text-center p-4 relative',
+                style: { minHeight: '200px' },
+              })}
+            >
+              <input {...getInputProps()} />
+              <p className="text-sm font-medium text-light-text dark:text-dark-text">
+                قم بالسحب والإفلات هنا، أو انقر لتحديد الملفات
+              </p>
+              {imagePreviewUrl && (
+                <img
+                  src={imagePreviewUrl}
+                  alt="Preview"
+                  className="absolute top-0 left-0 right-0 bottom-0 w-full h-full object-cover rounded-lg"
+                />
+              )}
+            </div>
+          </div>
         </div>
-        <div className="md:col-span-6">
-          <CategorySelect
-            categories={categories}
-            selectedCategoryIds={selectedCategoryIds}
-            onCategoryChange={setSelectedCategoryIds}
-          />
-        </div>
+
+        {/* Content editor */}
         <div>
           <label
             htmlFor="content"
@@ -128,6 +152,8 @@ const CreatePost = () => {
           </label>
           <TextEditor onContentChange={handleContentChange} />
         </div>
+
+        {/* Submit button */}
         <div className="flex justify-center mt-4">
           <button
             type="submit"
