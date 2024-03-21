b9551dcd879c8a10ef13b3350ed1b8e8ba60388a
FadiBadarni <fadybd1@gmail.com>
2024-03-21 21:00:14 +0200

    Add loading and error handling to RelatedPosts component

--- src/components/post/related/index.tsx
+++ src/components/post/related/index.tsx
@@ -1,8 +1,14 @@
+import {
+  ExclamationCircleIcon,
+  FaceFrownIcon,
+} from '@heroicons/react/24/outline';
 import PostItem from 'components/shared/PostItem';
+import PostItemSkeleton from 'components/shared/PostItemSkeleton';
 import { useAppDispatch } from 'hooks/useAppDispatch';
 import React, { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { fetchRelatedPosts } from 'store/post/postActions';
+import { LoadingStatus } from 'store/shared/commonState';
 import { RootState } from 'store/store';
 
 interface RelatedPostsProps {
@@ -14,27 +20,44 @@ const RelatedPosts: React.FC<RelatedPostsProps> = ({ postId }) => {
   const { data, status, error } = useSelector(
     (state: RootState) => state.posts.relatedPosts
   );
+  const ITEMS_PER_PAGE = 3;
+  const SKELETON_COUNT = 3;
 
   useEffect(() => {
-    dispatch(fetchRelatedPosts({ relatedPostId: postId, page: 0, size: 10 }));
+    dispatch(
+      fetchRelatedPosts({
+        relatedPostId: postId,
+        page: 0,
+        size: ITEMS_PER_PAGE,
+      })
+    );
   }, [dispatch, postId]);
-
-  if (status === 'loading') return <div>Loading related posts...</div>;
-  if (error) return <div>Error fetching related posts: {error}</div>;
 
   return (
     <div className="related-posts-container mb-8">
       <h2 className="text-xl text-center font-semibold mb-4">
         مواضيع يمكن تعبي عينك
       </h2>
-      {data.items && data.items.length > 0 ? (
-        data.items.map((post) => (
-          <>
-            <PostItem key={post.id} post={post} />
-          </>
+      {status === LoadingStatus.Loading ? (
+        Array.from({ length: SKELETON_COUNT }, (_, index) => (
+          <PostItemSkeleton key={index} />
         ))
+      ) : error ? (
+        <div className="flex flex-col items-center justify-center">
+          <ExclamationCircleIcon className="w-8 h-8 text-red-500" />
+          <p className="mt-2">
+            يا دنيا... في مشكلة جابتلنا العيد بجلب المواضيع المتعلقة: {error}
+          </p>
+        </div>
+      ) : data.items && data.items.length > 0 ? (
+        data.items.map((post) => <PostItem key={post.id} post={post} />)
       ) : (
-        <p>والله ما لقينا إشي يناسب ذوقك الرفيع! جرب مرة ثانية.</p>
+        <div className="flex flex-col items-center justify-center">
+          <FaceFrownIcon className="w-8 h-8 text-gray-400" />
+          <p className="mt-2">
+            والله ما لقينا إشي يناسب ذوقك الرفيع! جرب بعدين.
+          </p>
+        </div>
       )}
     </div>
   );
