5b994759f4b9c3c0e79ea1698165cedfea096ee9
FadiBadarni <fadybd1@gmail.com>
2024-03-26 12:24:15 +0200

    Add icons, locale, and navigation to PostCard component

--- src/components/all_posts/PostCard.tsx
+++ src/components/all_posts/PostCard.tsx
@@ -4,6 +4,9 @@ import { format, parseISO } from 'date-fns';
 import { useSelector } from 'react-redux';
 import { RootState } from 'store/store';
 import { determineTextDirection } from 'utils/textDirection';
+import { CalendarIcon, ClockIcon, UserIcon } from '@heroicons/react/24/outline';
+import { ar } from 'date-fns/locale';
+import { useNavigate } from 'react-router-dom';
 
 interface PostCardProps {
   post: LightPost;
@@ -11,6 +14,7 @@ interface PostCardProps {
 
 const PostCard: React.FC<PostCardProps> = ({ post }) => {
   const currentTheme = useSelector((state: RootState) => state.theme.theme);
+  const navigate = useNavigate();
 
   const [imageUrl, setImageUrl] = useState(
     post.mainImageUrl || '/missing-image-dark.png'
@@ -21,25 +25,32 @@ const PostCard: React.FC<PostCardProps> = ({ post }) => {
     else setImageUrl('/missing-image-dark.png');
   };
 
-  const formattedPublishedDate = format(
-    parseISO(post.publishedAt),
-    'd MMMM yyyy'
-  );
+  const handlePostClick = () => {
+    navigate(`/posts/${post.id}`);
+  };
+
+  const handleCategoryClick = (slug: string, e: React.MouseEvent) => {
+    e.stopPropagation();
+    navigate(`/category/${slug}`);
+  };
 
   return (
-    <div className="flex flex-col rounded-lg shadow-lg overflow-hidden bg-white dark:bg-dark-layer transition duration-300 ease-in-out transform hover:-translate-y-1 hover:shadow-xl relative">
+    <article
+      onClick={handlePostClick}
+      className="flex flex-col rounded-lg shadow-lg overflow-hidden bg-white dark:bg-dark-layer transition duration-300 ease-in-out transform hover:-translate-y-1 hover:shadow-xl cursor-pointer"
+    >
       <div className="flex-shrink-0">
         <img
-          className="h-40 w-full object-cover"
+          className="h-32 sm:h-40 w-full object-cover"
           src={imageUrl}
           alt={post.title}
           onError={handleImageError}
         />
       </div>
-      <div className="flex-1 p-4 flex flex-col justify-between">
+      <div className="flex-1 p-3 flex flex-col justify-between">
         <div>
           <h3
-            className={`text-xl font-semibold text-gray-900 dark:text-white line-clamp-1 overflow-hidden overflow-ellipsis ${
+            className={`text-lg font-semibold text-gray-900 dark:text-white line-clamp-1 ${
               determineTextDirection(post.title) === 'rtl'
                 ? 'text-right'
                 : 'text-left'
@@ -48,19 +59,35 @@ const PostCard: React.FC<PostCardProps> = ({ post }) => {
           >
             {post.title}
           </h3>
-          <p className="mt-1 text-sm text-gray-500 dark:text-gray-400">
-            بواسطة {post.author} - {formattedPublishedDate}
-          </p>
+
+          <div className="flex justify-around items-center text-neutral-500 dark:text-neutral-400 sm:text-xs mt-2 sm:h-12 h-8">
+            <div className="flex flex-row sm:flex-col items-center justify-center">
+              <UserIcon className="h-5 w-5 mb-1 sm:ml-0 ml-2" />
+              <span className="text-center">{post.author}</span>
+            </div>
+            <div className="flex flex-row sm:flex-col items-center">
+              <CalendarIcon className="h-5 w-5 mb-1 sm:ml-0 ml-2" />
+              <time dateTime={post.publishedAt}>
+                {format(parseISO(post.publishedAt), 'dd MMMM, yyyy', {
+                  locale: ar,
+                })}
+              </time>
+            </div>
+            {post.readingTime && (
+              <div className="flex flex-row sm:flex-col items-center">
+                <ClockIcon className="h-5 w-5 mb-1 sm:ml-0 ml-2" />
+                <span>{post.readingTime} دَ قراءة </span>
+              </div>
+            )}
+          </div>
         </div>
-        <div className="mt-2">
-          <p className="text-gray-500 text-sm">
-            وقت القراءة: {post.readingTime} دقيقة
-          </p>
-          <div className="flex flex-wrap mt-1">
+        <div className="flex flex-col justify-start items-start sm:mt-4 mt-2">
+          <div className="flex flex-wrap gap-2 w-full">
             {post.categoryDetails.map((category: CategoryDetail) => (
               <span
                 key={category.id}
-                className="mr-2 text-xs font-medium bg-blue-100 text-blue-800 px-2 py-1 rounded-full"
+                onClick={(e) => handleCategoryClick(category.slug, e)}
+                className="inline-flex items-center justify-center rounded-md px-2 py-1 text-xs font-medium bg-neutral-400/50 dark:bg-dark-border text-light-text dark:text-dark-text hover:bg-light-primary dark:hover:bg-dark-primary cursor-pointer transition-colors duration-200 ease-in-out"
               >
                 {category.name}
               </span>
@@ -68,7 +95,7 @@ const PostCard: React.FC<PostCardProps> = ({ post }) => {
           </div>
         </div>
       </div>
-    </div>
+    </article>
   );
 };
 
